ce54d6b24169f8c0d828c2c6888eba819116cce7
Owner <sandhu09arsh@gmail.com>
2025-06-12 23:31:37 -0400

    Fixed logo

--- src/components/ui/custom/Header.jsx
+++ src/components/ui/custom/Header.jsx
@@ -30,52 +30,52 @@ function Header() {
   const login = useGoogleLogin({
     onSuccess: (codeResp) => GetUserProfile(codeResp),
     onError: (error) => console.log(error)
-  })
+})
 
-  const GetUserProfile = (tokenInfo) => {
-    axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${tokenInfo?.access_token}`, {
+const GetUserProfile = (tokenInfo) => {
+  axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${tokenInfo?.access_token}`, {
       headers: {
-        Authorization: `Bearer ${tokenInfo?.access_token}`,
-        Accept: 'Application/json'
+          Authorization: `Bearer ${tokenInfo?.access_token}`,
+          Accept: 'Application/json'
       }
-    }).then((resp) => {
+  }).then((resp) => {
       console.log(resp);
       localStorage.setItem('user', JSON.stringify(resp.data));
       setOpenDialog(false);
       window.location.reload()
-    })
-  }
+  })
+}
 
   return (
     <div className='p-3 shadow-sm flex justify-between items-center px-5'>
       <a href='/'>
-        <Button variant="ghost" className="p-0 hover:bg-transparent">
-          <div className="w-[200px] xs:w-[160px] md:w-[180px] h-auto aspect-[1750/398]"> {/* 4.44:1 ratio */}
-            <img
-              src='/logo.png'
-              className="w-full h-full object-contain"
-              alt="Logo"
-            />
-          </div>
-        </Button>
-      </a>
+  <Button variant="ghost" className="p-0 hover:bg-transparent">
+    <div className="w-[200px] xs:w-[160px] md:w-[180px] h-auto aspect-[1750/398]"> {/* 4.44:1 ratio */}
+      <img 
+        src='/logo.png' 
+        className="w-full h-full object-contain" 
+        alt="Logo"
+      />
+    </div>
+  </Button>
+</a>
       <div>
         {user ?
           <div className='flex items-center gap-3'>
             <a href='/create-trip'>
-              <Button variant="outline"
-                className="rounded-full">Create Trip</Button>
+            <Button variant="outline"
+            className="rounded-full">Create Trip</Button>
             </a>
             <a href='/my-trips'>
-              <Button variant="outline"
-                className="rounded-full">My Trips</Button>
+            <Button variant="outline"
+            className="rounded-full">My Trips</Button>
             </a>
             <Popover>
               <PopoverTrigger>
                 <img src={user?.picture} className='h-[35px] w-[35px] rounded-full' />
               </PopoverTrigger>
               <PopoverContent>
-                <h2 className='cursor-pointer' onClick={() => {
+                <h2 className='cursor-pointer' onClick={()=>{
                   googleLogout();
                   localStorage.clear();
                   window.location.reload();
@@ -84,36 +84,36 @@ function Header() {
             </Popover>
           </div>
           :
-          <Button onClick={() => setOpenDialog(true)}>Sign In</Button>
+          <Button onClick={()=>setOpenDialog(true)}>Sign In</Button>
         }
       </div>
       <Dialog open={openDialog} onOpenChange={setOpenDialog}>
 
-        <DialogContent>
-          <DialogHeader>
+                <DialogContent>
+                    <DialogHeader>
 
-            <DialogDescription className="relative">
-              <img
-                src="/logo.png"
-                alt="Logo"
-                className="absolute top-0 right-0 w-12 h-auto"
-              />
-              <h2 className='font-bold text-lg mt-7'>Sign-In With Google</h2>
-              <p>Securely sign-in to the app with Google authentication</p>
+                        <DialogDescription>
+                            <img 
+                              src='/logo.png'
+                              className="w-full h-full object-contain"
+                              alt="Logo" 
+                            />
+                            <h2 className='font-bold text-lg mt-7'>Sign-In With Google</h2>
+                            <p>Securely sign-in to the app with Google authentication</p>
 
-              <Button
+                            <Button
 
-                onClick={login}
-                className='w-full mt-5 flex gap-2 items-center'>
+                                onClick={login}
+                                className='w-full mt-5 flex gap-2 items-center'>
 
-                <FcGoogle className='h-7 w-7' />
-                Sign-In With Google
+                                <FcGoogle className='h-7 w-7' />
+                                Sign-In With Google
 
-              </Button>
-            </DialogDescription>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
+                            </Button>
+                        </DialogDescription>
+                    </DialogHeader>
+                </DialogContent>
+            </Dialog>
     </div>
   )
 }
